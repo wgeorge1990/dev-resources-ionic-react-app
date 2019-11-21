472d05ac245f7f79cf34751ef2abe991399b1298
wgeorge1990 <williameverettgeorge@icloud.com>
2019-11-20 20:49:55 -0500

    ionic build, add capacitor, build ios instance, cpy ionic build instance to ios application using ionic

---
+++ ios/App/public/static/js/5.911020f0.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpDevResources=this.webpackJsonpDevResources||[]).push([[5],{190:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return c}));var o=e(25),c=(e(3),function(){var n=window;n.addEventListener("statusTap",(function(){Object(o.g)((function(){var t=n.innerWidth,e=n.innerHeight,c=document.elementFromPoint(t/2,e/2);if(c){var i=c.closest("ion-content");i&&i.componentOnReady().then((function(){Object(o.m)((function(){return i.scrollToTop(300)}))}))}}))}))})}}]);
+//# sourceMappingURL=5.911020f0.chunk.js.map
